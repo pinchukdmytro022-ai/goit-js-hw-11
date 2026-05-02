2e64429f9052f1e3a3b8c089360c94dfb312ba34
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-05-02 23:01:16 +0300

    исправление в функциях рендера

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -6,8 +6,20 @@ let lightbox;
 export const createGalleryCardTemplate = img => `
   <li class="gallery-card">
     <a class="gallery-link" href="${img.largeImageURL}">
-      <img class="gallery-img" src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+      <img 
+        class="gallery-img" 
+        src="${img.webformatURL}" 
+        alt="${img.tags}" 
+        loading="lazy" 
+      />
     </a>
+
+    <div class="card-info">
+      <p><b>Likes</b> ${img.likes}</p>
+      <p><b>Views</b> ${img.views}</p>
+      <p><b>Comments</b> ${img.comments}</p>
+      <p><b>Downloads</b> ${img.downloads}</p>
+    </div>
   </li>
 `;
 
